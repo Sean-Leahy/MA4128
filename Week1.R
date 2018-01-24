de84cbf13d930e1ded27aeee2f440ecf2b9e2281
Sean-Leahy <35765972+Sean-Leahy@users.noreply.github.com>
2018-01-24 16:56:40 +0000

    Update Week1.R

--- Week1.R
+++ Week1.R
@@ -1,20 +1,54 @@
+install.packages("dplyr")
+library(dplyr)
+sessionInfo()
+
+mtcars
+mtcars %>% head()
+
+dim(mtcars)
+mtcars %>% nrow()
+mtcars %>% ncol()
+mtcars %>% names()
+mtcars %>% class()
+
+
+head(iris)
 summary(head(iris))
 mtcars %>% summary()
-
 iris %>% summary()
 iris %>% glimpse()
 
-filter(iris, Species == "setosa")
 iris3 <- iris %>% filter(Sepal.Length > 2)
-dim(iris3)
-iris3 <- iris %>% filter(Sepal.Length > 5.5)
-dim(iris3)
-iris3 <- iris %>% filter(Sepal.Length > 5.2)
-dim(iris3)
-iris3 <- iris %>% filter(Sepal.Length > 5.)
-dim(iris3)
-iris3 <- iris %>% filter(Sepal.Length > 5.4)
-dim(iris3)
-iris3 <- iris %>% filter(Sepal.Length > 5.6)
-dim(iris3)
+dim(iris3) 
 history(100)
+
+iris %>% group_by(Species) %>% summarize(MSL = mean(Sepal.Length))
+
+iris %>% group_by(Species) %>% summarize(VarSL = var(Sepal.Length))
+
+iris %>% group_by(Species) %>% summarize(SDSL = sd(Sepal.Length))
+
+iris %>% group_by(Species) %>% summarize(MedSL = median(Sepal.Length))
+
+iris %>% group_by(Species) %>% summarize(MedSL = median(Sepal.Length),
+                                         VarSL = var(Sepal.Length),
+                                         SDSL = sd(Sepal.Length),
+                                         MSL = mean(Sepal.Length))
+
+
+mtcars %>% group_by(cyl) %>% summarize(MMPG = mean(mpg),
+                                       VarMP = var(mpg),                  
+                                       SDMPG = sd(mpg),                 
+                                       MedMPG = median(mpg)) 
+
+mtcars %>% group_by(vs,am) %>% summarize(MedianMPG = median(mpg))
+
+mtcars %>% select(1:4) %>% head(4)
+
+
+mtcars %>% mutate( sqrtMPG= sqrt(mpg)) %>% head(4)
+
+mtcars %>% mutate( sqrtQSEC= sqrt(qsec)) %>% head(5)
+
+mtcars %>% mutate(sqrtMPG= sqrt(mpg), logMPG= log(mpg)) 
+%>% select(mpg, sqrtMPG, logMPG, everything() ) %>% head(4)
